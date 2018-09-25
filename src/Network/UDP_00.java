7336edea18601f06c542865a9492c5e51ba32104
capttwigg <mike@familien-jahn.dk>
2018-09-25 18:46:31 +0200

    chat with gui

--- src/Network/UDP_00.java
+++ src/Network/UDP_00.java
@@ -5,8 +5,12 @@ package Network;
  * Write a description of class UDP_0 here.
  */
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class UDP_00 {
   public static void main(String args[]) throws Exception {
@@ -16,27 +20,47 @@ public class UDP_00 {
     DatagramSocket receivingSocket = new DatagramSocket(6710);
     DatagramSocket sendingSocket = new DatagramSocket();
     InetAddress IPAddress = InetAddress.getByName("127.0.0.1");
-    byte[] data = new byte[1024];
+
+   // Gui gui = new Gui();
+
+    threadReceive(receivingSocket);
+
+
     while (true) {
+      byte[] data = new byte[1024];
 
-      DatagramPacket receivePacket = new DatagramPacket(data, 33);
-      receivingSocket.receive(receivePacket);
-      sentence = new String(receivePacket.getData());
-      int size = receivePacket.getLength();
-      System.out.println("FROM SERVER size:" + size);
-      System.out.println("FROM SERVER:" + sentence);
-
-      System.out.println("Please type you message: ");
+      System.out.print("6710: ");
       sentence = inFromUser.readLine();
       length = sentence.length();
       data = sentence.getBytes();
       DatagramPacket sendPacket = new DatagramPacket(data, length, IPAddress, 6701);
       sendingSocket.send(sendPacket);
-      sentence = " ";
-      data = sentence.getBytes();
+
     }
 
   }
+
+  static void threadReceive(DatagramSocket receivingSocket) {
+    Thread thread = new Thread(() -> {
+      while (true) {
+        byte[] data = new byte[1024];
+        String receiveSentence;
+        try {
+          DatagramPacket receivePacket = new DatagramPacket(data, data.length);
+          receivingSocket.receive(receivePacket);
+          receiveSentence = new String(receivePacket.getData());
+
+
+          System.out.println("\nFROM SERVER:" + receiveSentence);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+    thread.start();
+  }
+
+
 }
 
 
--- src/Network/UDP_11.java
+++ src/Network/UDP_11.java
@@ -1,43 +1,39 @@
 package Network;
 /**
  * Write a description of class UDP_1 here.
- *
  */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.Scanner;
 
-public class UDP_11
-{
-  public static void main(String args[]) throws Exception
-  {
+public class UDP_11 {
+  public static void main(String args[]) throws Exception {
     String sentence;
     int length;
 
-      //BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-      Scanner inFromKbd = new Scanner(System.in);
-      DatagramSocket receivingSocket = new DatagramSocket(6701);
-      DatagramSocket sendingSocket = new DatagramSocket();
-      InetAddress IPAddress = InetAddress.getByName("127.0.0.1");
-      byte[] data = new byte[1024];
-    while (true) {
+    BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+   // Scanner inFromKbd = new Scanner(System.in);
+    DatagramSocket receivingSocket = new DatagramSocket(6701);
+    DatagramSocket sendingSocket = new DatagramSocket();
+    InetAddress IPAddress = InetAddress.getByName("127.0.0.1");
 
-      System.out.println("Please type you message: ");
-      //sentence = inFromUser.readLine();
-      sentence = inFromKbd.nextLine();
+    //Gui gui = new Gui();
+    UDP_00.threadReceive(receivingSocket);
+
+    while (true) {
+      byte[] data = new byte[1024];
+
+      System.out.print("6701: ");
+      sentence = inFromUser.readLine();
+      //sentence = inFromKbd.nextLine();
       length = sentence.length();
       data = sentence.getBytes();
       DatagramPacket sendPacket = new DatagramPacket(data, length, IPAddress, 6710);
       sendingSocket.send(sendPacket);
-      sentence = "                               ";
-      data = sentence.getBytes();
 
-      DatagramPacket receivePacket = new DatagramPacket(data, data.length);
-      receivingSocket.receive(receivePacket);
-      sentence = new String(receivePacket.getData());
-      System.out.println("FROM SERVER:" + sentence);
 
     }
   }
